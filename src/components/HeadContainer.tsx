42277aab6248bb7fd816dcfca81f88d91ad2413a
N1K100a <chiqovani.nika01@gmail.com>
2023-01-16 22:45:03 +0400

    simple hovers and fix

--- src/components/HeadContainer.tsx
+++ src/components/HeadContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Dark from "../Icons/dayNight/Dark";
 import Light from "../Icons/dayNight/Light";
